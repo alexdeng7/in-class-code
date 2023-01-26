38d803d80c1325fdf45dfc3ad60ffc3f77bc18cb
alexdeng7 <alex.deng@yale.edu>
2023-01-25 22:09:56 -0500

    Complex Additions

--- main.c
+++ main.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include "factorial.h"
 
-typedef char* string;
+typedef char *string;
 
 int main(int argc, string argv[])
 {
     int n;
     char name[13];
     string exeName = argv[0];
-    scanf("%[^\n]s", name);
+    scanf("%12[^\n]s", name);
     printf("%s\n", exeName);
+    printf("%s\n", name);
     for (int i = 0; i < argc; i++)
     {
         printf("Command line argument %d is %s\n", i + 1, argv[i]);
     }
-    printf("%12s\n", name);
     while (scanf("%d", &n) > 0)
     {
         long fac = factorial(n);
